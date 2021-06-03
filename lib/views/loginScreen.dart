96ece5ffb7f078feeabaca0392e86cdcf00f52da
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-06-04 00:25:23 +0530

    up

--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/components/backgroundPainter.dart';
 import 'package:quiz_app/components/loginComponent.dart';
 import 'package:quiz_app/constants/ui_constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LogInScreen extends StatefulWidget {
   @override
@@ -26,7 +27,6 @@ class _LogInScreenState extends State<LogInScreen> {
     });
     return false;
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -58,13 +58,71 @@ class _LogInScreenState extends State<LogInScreen> {
                 SizedBox(
                   height: UIConstants.fitToHeight(75, context),
                 ),
-                Image.asset('assets/images/logo.png',height: UIConstants.fitToHeight(200, context),),
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: UIConstants.fitToHeight(180, context),
+                ),
                 SizedBox(
-                  height: UIConstants.fitToHeight(50, context),
+                  height: UIConstants.fitToHeight(30, context),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 40.0),
                   child: LoginButtonComponentAndroid(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          InkWell(
+                            child: Text("Privacy Policy"),
+                            onTap: () async {
+                              if (await canLaunch(
+                                  "https://quizaddaplus.tk/privacy")) {
+                                await launch("https://quizaddaplus.tk/privacy");
+                              } else {
+                                print(
+                                    'Could not launch https://quizaddaplus.tk/privacy');
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          InkWell(
+                            child: Text("Refund Policy"),
+                            onTap: () async {
+                              if (await canLaunch(
+                                  "https://quizaddaplus.tk/refund")) {
+                                await launch("https://quizaddaplus.tk/refund");
+                              } else {
+                                print(
+                                    'Could not launch https://quizaddaplus.tk/refund');
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      InkWell(
+                        child: Text("Terms & Condition"),
+                        onTap: () async {
+                          if (await canLaunch(
+                              "https://quizaddaplus.tk/terms")) {
+                            await launch("https://quizaddaplus.tk/terms");
+                          } else {
+                            print(
+                                'Could not launch https://quizaddaplus.tk/terms');
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
